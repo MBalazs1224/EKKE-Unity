8c04bdd7b1376b0241d55989554b900fcb6a811d
Daniella1818 <113984678+Daniella1818@users.noreply.github.com>
2023-04-11 08:25:50 +0200

    Explodingcar

---
+++ EKKE-Unity/Assets/Enemy/State Machine/ExplodingCar/CarAttackState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAttackState : EnemyBaseState
+{
+    public override void EnterState(EnemyStateManager manager, GameObject gameObject, Character player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Tick()
+    {
+        throw new System.NotImplementedException();
+    }
+}
