6a9e526ae8597e12606e10747be7f5a8f61bba2f
Daniella1818 <113984678+Daniella1818@users.noreply.github.com>
2023-05-02 08:22:24 +0200

    Retek

--- EKKE-Unity/Assets/Enemy/State Machine/ExplodingCar/CarSearchState.cs
+++ EKKE-Unity/Assets/Enemy/State Machine/ExplodingCar/CarSearchState.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class CarSearchState : EnemyBaseState
 {
     int maxDetectionDistance = 5;
-    Animator animator;
+    Animator anim;
     float maxXRight;
     float maxXLeft;
     bool moveRight = true;
+<<<<<<< Updated upstream
     SpriteRenderer sr;
+=======
+    Rigidbody2D rb;
+>>>>>>> Stashed changes
     public override void EnterState(EnemyStateManager manager, GameObject gameObject, Character player)
     {
         stateManager = manager;
         currentObject = gameObject;
+<<<<<<< Updated upstream
         maxXLeft = currentObject.transform.position.x - 5;
         maxXRight = currentObject.transform.position.x + 5;
         this.player = player;
         Debug.Log("Exploding car entered search state!");
         animator = currentObject.GetComponent<Animator>();
         sr = currentObject.GetComponent<SpriteRenderer>();
+=======
+        maxXLeft = currentObject.transform.position.x - 2;
+        maxXRight = currentObject.transform.position.x + 2;
+        this.player = player;
+        Debug.Log("Exploding car entered search state!");
+        anim = currentObject.GetComponent<Animator>();
+>>>>>>> Stashed changes
     }
 
     public override void Tick()
@@ -28,16 +40,25 @@ public class CarSearchState : EnemyBaseState
         {
             if (moveRight)
             {
+<<<<<<< Updated upstream
                 currentObject.transform.Translate(new Vector3(.2f, 0));
                 if (currentObject.transform.position.x >= maxXRight)
                 {
                     moveRight = false;
                     animator.Play("Turn_Left");
                     sr.flipX = true;
+=======
+                currentObject.transform.Translate(new Vector3(.01f, 0f));
+                if (currentObject.transform.position.x >= maxXRight) 
+                {
+                    moveRight = false;
+                    anim.SetBool("Left", true);
+>>>>>>> Stashed changes
                 }
             }
             else
             {
+<<<<<<< Updated upstream
                 
                 currentObject.transform.Translate(new Vector3(-.2f, 0));
                 if (currentObject.transform.position.x <= maxXLeft)
@@ -45,6 +66,12 @@ public class CarSearchState : EnemyBaseState
                     moveRight = true;
                     animator.Play("Turn_Right");
                     sr.flipX = false;
+=======
+                currentObject.transform.Translate(new Vector3(-.01f, 0f));
+                if (currentObject.transform.position.x <= maxXLeft)
+                {
+                    moveRight = true;
+>>>>>>> Stashed changes
                 }
             }
         }
