5c714f172c35cc0d7f6b647e3005248fca9b55c0
MBalazs1224 <127351715+MBalazs1224@users.noreply.github.com>
2023-05-09 09:56:25 +0200

    Skybox és camera update

--- EKKE-Unity/Assets/Camera/Scripts/CameraController.cs
+++ EKKE-Unity/Assets/Camera/Scripts/CameraController.cs
@@ -9,6 +9,8 @@ public class CameraController : MonoBehaviour
     Transform target;
     Vector3 velocity = Vector3.zero;
     [SerializeField]float dampTime = 0.5f;
+    [SerializeField]
+    float camHeight = 5;
     void Start()
     {
         cam = Camera.main;
@@ -21,7 +23,7 @@ public class CameraController : MonoBehaviour
         if (target)
         {
             Vector3 point = cam.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 delta = target.position + new Vector3 (0,camHeight) - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }   
