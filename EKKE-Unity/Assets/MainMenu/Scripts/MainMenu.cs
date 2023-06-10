9706b7eeb376869f97e1b10664cd0ed9ce3095fd
MBalazs1224 <127351715+MBalazs1224@users.noreply.github.com>
2023-06-10 21:51:30 +0200

    Loading screen

--- EKKE-Unity/Assets/MainMenu/Scripts/MainMenu.cs
+++ EKKE-Unity/Assets/MainMenu/Scripts/MainMenu.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    GameObject loading;
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // "Game scene"
+        this.gameObject.SetActive(false);
+        loading.SetActive(true);
+        VideoPlayer loadingVP = loading.GetComponent<VideoPlayer>();
+        loadingVP.loopPointReached += (_) =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // "Game scene"
+        };
+    }
+    IEnumerator LoadScene()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        op.allowSceneActivation = false;
+        while (op.progress < .9f)
+        {
+            yield return null;
+        }
+        op.allowSceneActivation = true;
     }
     public void Quit()
     {
